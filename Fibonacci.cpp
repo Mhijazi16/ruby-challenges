dc41f1a8c682db3ac41859e793d769debf5a1128
Mhijazi16 <gultimate0@gmail.com>
2024-09-05 11:49:55 +0300

    create get_next_number() takes in vector<int> and returns the next number in the sequence

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -3,9 +3,14 @@
 
 using namespace std; 
 
+int get_next_number(vector<int> seq){
+  int last = seq.back(); 
+  int second_last = *(seq.end()-2);
+  return last + second_last; 
+}
+
 int main(){ 
-
   vector<int> sequence = {0,1,1,2,3,5,8};
-
+  cout << get_next_number(sequence) << endl;
   return 0; 
 }
